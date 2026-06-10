2c8dcd6cb05cc4b309ebb10509021c2fa3133761
rxits <rxits@proton.me>
2026-06-11 01:12:34 +0530

    wire new sources into registry

--- src/lib/sources/index.ts
+++ src/lib/sources/index.ts
@@ -2,5 +2,9 @@ import type { JobSource } from "../types";
 import { remoteOkSource } from "./remoteok";
 import { hnHiringSource } from "./hn-hiring";
 import { hnJobsSource } from "./hn-jobs";
+import { remotiveSource } from "./remotive";
+import { jobicySource } from "./jobicy";
+import { himalayasSource } from "./himalayas";
+import { wwrSource } from "./wwr";
 
-export const sources: JobSource[] = [hnHiringSource, remoteOkSource, hnJobsSource];
+export const sources: JobSource[] = [hnHiringSource, remoteOkSource, hnJobsSource, remotiveSource, jobicySource, himalayasSource, wwrSource];
